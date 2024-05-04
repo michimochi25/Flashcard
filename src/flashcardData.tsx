014fced920e3ef3224cc5b7105ff4c29ae814f51
Giselle <cryponiess@gmail.com>
2024-05-04 19:16:43 +1000

    Changed into TypeScript file

---
+++ src/flashcardData.tsx
@@ -0,0 +1,134 @@
+export const kanjis = [
+    {
+        week: "1",
+        materials: [
+            { romaji: "shima", meaning: "island", kanji: "島" },
+            { romaji: "hantou", meaning: "peninsula", kanji: "半島" },
+            { romaji: "shimada", meaning: "shimada", kanji: "島田" },
+            { romaji: "kojima", meaning: "kojima", kanji: "小島" },
+            { romaji: "tajima", meaning: "tajima", kanji: "田島" },
+            { romaji: "shimamoto", meaning: "shimamoto", kanji: "島本" },
+            { romaji: "taira", meaning: "flat", kanji: "平ら" },
+            { romaji: "washoku", meaning: "japanese food", kanji: "和食" },
+            { romaji: "dentouteki", meaning: "traditional", kanji: "伝統的" },
+            { romaji: "tsuateru", meaning: "to report", kanji: "伝える" },
+            { romaji: "tetsudau", meaning: "to help", kanji: "手伝う" },
+            { romaji: "kion", meaning: "temperature", kanji: "気温" },
+            { romaji: "kokoro ga atatakai", meaning: "warm heart", kanji: "心が温かい" },
+            { romaji: "sa", meaning: "difference", kanji: "差" }
+        ]
+    },
+    {
+        week: "2",
+        materials: [
+            { romaji: "bijutsukan", meaning: "museum", kanji: "美術館" },
+            { romaji: "utsukushii", meaning: "beautiful", kanji: "美しい" },
+            { romaji: "senshu", meaning: "athlete", kanji: "選手" },
+            { romaji: "erabu", meaning: "to choose", kanji: "選ぶ" },
+            { romaji: "zannen", meaning: "disappointing", kanji: "残念" },
+            { romaji: "nokoru", meaning: "to leave out", kanji: "残る" },
+            { romaji: "tatemono", meaning: "building", kanji: "建物" },
+            { romaji: "kenchiku", meaning: "architecture", kanji: "建築" },
+            { romaji: "tateru", meaning: "to build", kanji: "建てる" },
+            { romaji: "kakokei", meaning: "past tense", kanji: "過去形" },
+            { romaji: "ningyou", meaning: "doll", kanji: "人形" },
+            { romaji: "katachi", meaning: "shape", kanji: "形" },
+            { romaji: "mokuteki", meaning: "aim/goal", kanji: "目的" }
+        ]
+    },
+    {
+        week: "3",
+        materials: [
+            { romaji: "tokuni", meaning: "especially", kanji: "特に" },
+            { romaji: "tokubetsu", meaning: "special", kanji: "特別" },
+            { romaji: "shi", meaning: "city", kanji: "市" },
+            { romaji: "yokohamashi", meaning: "yokohama city", kanji: "横浜市" },
+            { romaji: "ichiba", meaning: "market", kanji: "市場" },
+            { romaji: "kabushiki shijou", meaning: "stock market", kanji: "株式市場" },
+            { romaji: "daitoshi", meaning: "big city", kanji: "大都市" },
+            { romaji: "shousetsu", meaning: "novel", kanji: "小説" },
+            { romaji: "shousetsuka", meaning: "novelist", kanji: "小説家" },
+            { romaji: "setsumei (suru)", meaning: "to explain", kanji: "説明する" },
+            { romaji: "kaiketsu (suru)", meaning: "to solve", kanji: "解決する" },
+            { romaji: "kimeru", meaning: "to decide", kanji: "決める" },
+            { romaji: "wakareru", meaning: "to break up", kanji: "別れる" },
+            { romaji: "kodomotachi", meaning: "children", kanji: "子供達" },
+            { romaji: "joutatsu (suru)", meaning: "to improve", kanji: "上達する" },
+            { romaji: "tomodachi", meaning: "friend", kanji: "友達" },
+            { romaji: "kankaku", meaning: "sense", kanji: "感覚" },
+            { romaji: "oboeru", meaning: "to memorize", kanji: "覚える" }
+        ]
+    },
+    {
+        week: "4",
+        materials: [
+            { romaji: "hatsuon", meaning: "pronunciation", kanji: "発音" },
+            { romaji: "hattatsu (suru)", meaning: "to develop", kanji: "発達する" },
+            { romaji: "happyou", meaning: "to present", kanji: "発表" },
+            { romaji: "hoka ni", meaning: "in addition to", kanji: "他に" },
+            { romaji: "hoka niwa", meaning: "except for", kanji: "他には" },
+            { romaji: "bushu", meaning: "radical", kanji: "部首" },
+            { romaji: "kubi", meaning: "next", kanji: "首" },
+            { romaji: "onsei", meaning: "sound", kanji: "音声" },
+            { romaji: "koe", meaning: "voice", kanji: "声" },
+            { romaji: "shuukai", meaning: "meeting", kanji: "集会" },
+            { romaji: "atsumaru", meaning: "to gather", kanji: "集まる" },
+            { romaji: "kyoukyuu", meaning: "service/supply/provide", kanji: "供給" },
+            { romaji: "kodomo", meaning: "child", kanji: "子供" },
+            { romaji: "naku", meaning: "to cry", kanji: "泣く" },
+            { romaji: "ryoushin", meaning: "parents", kanji: "両親" },
+            { romaji: "hontou", meaning: "real, actual", kanji: "本当" },
+            { romaji: "ataru", meaning: "to hit, to win", kanji: "当たる" }
+        ]
+    },
+    {
+        week: "5",
+        materials: [
+            { romaji: "rikai (suru)", meaning: "to understand", kanji: "理解する" },
+            { romaji: "toku", meaning: "to solve", kanji: "解く" },
+            { romaji: "warau", meaning: "to laugh", kanji: "笑う" },
+            { romaji: "egao", meaning: "smile", kanji: "笑顔" },
+            { romaji: "bunpou", meaning: "grammar", kanji: "文法" },
+            { romaji: "houhou", meaning: "method", kanji: "方法" },
+            { romaji: "kenkouhou", meaning: "(how to) stay healthy", kanji: "健康法" },
+            { romaji: "chokusetsu", meaning: "directly", kanji: "直接" },
+            { romaji: "naosu", meaning: "to repair", kanji: "直す" },
+            { romaji: "kujou", meaning: "complaint", kanji: "苦情" },
+            { romaji: "kurushii", meaning: "suffering", kanji: "苦しい" },
+            { romaji: "nigate", meaning: "to be poor at", kanji: "苦手" },
+            { romaji: "joshu", meaning: "assistant", kanji: "助手" },
+            { romaji: "yobu", meaning: "to invite/call", kanji: "呼ぶ" },
+        ]
+    },
+    {
+        week: "6",
+        materials: [
+            { romaji: "gendai", meaning: "the present day", kanji: "現代" },
+            { romaji: "arawareru", meaning: "to appear", kanji: "現れる" },
+            { romaji: "bangumi", meaning: "program", kanji: "番組" },
+            { romaji: "kumitateru", meaning: "to assemble", kanji: "組み立てる" },
+            { romaji: "shoubu (suru)", meaning: "to have a match (n.) victory or defeat", kanji: "勝負する" },
+            { romaji: "katsu", meaning: "to win", kanji: "勝つ" },
+            { romaji: "seichou (suru)", meaning: "to grow", kanji: "成長する" },
+            { romaji: "sansei (suru)", meaning: "to agree", kanji: "賛成する" },
+            { romaji: "makeru", meaning: "to lose", kanji: "負ける" },
+        ]
+    },
+    {
+        week: "7",
+        materials: [
+            { romaji: "zettai (ni)", meaning: "absolutely/surely", kanji: "絶対" },
+            { romaji: "taezu", meaning: "continuously/always", kanji: "絶えず" },
+            { romaji: "ni taishite", meaning: "against", kanji: "に対して" },
+            { romaji: "rei wo suru", meaning: "to bow", kanji: "礼をする" },
+            { romaji: "rei wo iu", meaning: "to express gratitude", kanji: "礼を言う" },
+            { romaji: "reigi tadashii", meaning: "well-mannered", kanji: "礼儀正しい" },
+            { romaji: "houkou", meaning: "direction", kanji: "方向" },
+            { romaji: "ni mukau", meaning: "toward; for", kanji: "に向かう" },
+            { romaji: "sodatsu", meaning: "to grow up", kanji: "育つ" },
+            { romaji: "kosodate", meaning: "child rearing", kanji: "子育て" },
+            { romaji: "kyouiku", meaning: "education", kanji: "教育" }
+        ]
+    }
+
+]
